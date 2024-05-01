b56d47a26ad778e54913cfc788d68656285c1989
keifufu <github@keifufu.dev>
2024-05-01 21:07:54 +0200

    save screenshots locally and copy to clipboard

--- files/scripts/screenshot.sh
+++ files/scripts/screenshot.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# todo: position feh in overlay surface to display above fullscreen applications
+
 TOKEN=$(cat $NIXOS_SECRETS/yass_token);
 HOST=$(cat $NIXOS_SECRETS/yass_host);
 PPMIMG=$(mktemp --suffix ss.ppm)
 PNGIMG=$(mktemp --suffix ss.png)
 FILENAME=$(date '+%y-%m-%dT%H-%M-%S.png')
 FEHCLASS="screenshot-overlay";
+OUTPATH="/stuff/screenshots/"
 
 if [[ $(pgrep -c -f "screenshot.sh") -gt 1 ]]; then
   exit 0
@@ -21,17 +24,20 @@ if [[ -n "$region" ]]; then
     -c:v png -f image2pipe -pred 2 -compression_level 1 -  \
     | swappy -f - -o - > $PNGIMG
 
-  id=$(notify-send -t 999999 "Uploading screenshot..." --print-id)
+  cp "$PNGIMG" "$OUTPATH/$FILENAME"
+  wl-copy < "$PNGIMG"
 
-  RESPONSE=$(curl -s -w "%{http_code}" -X PUT $HOST/upload?filename=$FILENAME -H "Authorization: $TOKEN" -H "Content-Type: application/octet-stream" --data-binary "@$PNGIMG")
+  # id=$(notify-send -t 999999 "Uploading screenshot..." --print-id)
 
-  HTTP_STATUS="${RESPONSE: -3}"
-  BODY="${RESPONSE%${HTTP_STATUS}}"
+  # RESPONSE=$(curl -s -w "%{http_code}" -X PUT $HOST/upload?filename=$FILENAME -H "Authorization: $TOKEN" -H "Content-Type: application/octet-stream" --data-binary "@$PNGIMG")
 
-  if [[ "$HTTP_STATUS" -ge 200 && "$HTTP_STATUS" -lt 300 ]]; then
-    wl-copy "$BODY"
-    notify-send -t 5000 -r $id -i $PNGIMG "Done uploading"
-  else
-    notify-send -t 5000 -r $id -i $PNGIMG "Failed to upload: $HTTP_STATUS"
-  fi
+  # HTTP_STATUS="${RESPONSE: -3}"
+  # BODY="${RESPONSE%${HTTP_STATUS}}"
+
+  # if [[ "$HTTP_STATUS" -ge 200 && "$HTTP_STATUS" -lt 300 ]]; then
+  #   wl-copy "$BODY"
+  #   notify-send -t 5000 -r $id -i $PNGIMG "Done uploading"
+  # else
+  #   notify-send -t 5000 -r $id -i $PNGIMG "Failed to upload: $HTTP_STATUS"
+  # fi
 fi
