b8ed29fdaa90ddf04eb93cccba6661937e4d42d9
keifufu <github@keifufu.dev>
2024-05-12 13:13:26 +0200

    w

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -23,9 +23,19 @@
 
   services.logind.lidSwitch = "hibernate";
 
-  powerManagement = {
+  services.tlp = {
     enable = true;
-    cpuFreqGovernor = "powersave";
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
   };
-  services.tlp.enable = true;
 }
