e23d806d4035392c28c39ff43e172bddba01ae47
keifufu <github@keifufu.dev>
2024-05-13 11:16:26 +0200

    w

--- hosts/common/configuration/modules/ssh.nix
+++ hosts/common/configuration/modules/ssh.nix
@@ -1,13 +1,14 @@
+{ pkgs, ... }:
+
 {
   programs.ssh.startAgent = true;
-  pam.services = [
-    { name = "gnome_keyring"
-      text = ''
-        auth     optional    ${gnome3.gnome_keyring}/lib/security/pam_gnome_keyring.so
-        session  optional    ${gnome3.gnome_keyring}/lib/security/pam_gnome_keyring.so auto_start
-        password  optional    ${gnome3.gnome_keyring}/lib/security/pam_gnome_keyring.so
-      '';
-    }
-  ];
+  security.pam.services.gnome-keyring = {
+    name = "gnome-keyring";
+    text = ''
+      auth     optional    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome-keyring.so
+      session  optional    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+      password  optional    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+    '';
+  };
   services.gnome.gnome-keyring.enable = true;
 }
