8f4d1f2d0d17a20f57054e15040ee9bd6b3ad978
keifufu <github@keifufu.dev>
2024-05-16 11:16:07 +0200

    update server

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -22,10 +22,7 @@
 
   boot.kernelParams = [ "ip=192.168.2.111::192.168.2.1:255.255.255.0:server::none" ];
   boot.initrd = {
-    systemd.users.${vars.user} = {
-      uid = 1000;
-      shell = "/bin/cryptsetup-askpass";
-    };
+    systemd.users.root.shell = "/bin/cryptsetup-askpass";
     network = {
       enable = true;
       ssh = {
